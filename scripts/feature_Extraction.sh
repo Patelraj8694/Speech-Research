20157b66b7f1dcf7126a9da7ca598860cc162d24
Raj Patel <raj@allsite.ai>
2024-03-05 00:37:01 +1300

    Setting path for feature_extraction

--- scripts/feature_Extraction.sh
+++ scripts/feature_Extraction.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-for entry1 in `ls ../dataset/data/US_102/Normal/*.wav`;do
+for entry1 in `ls ../dataset/data/Normal/*.wav`;do
 fname=`basename $entry1 .wav`
 echo $fname 
-./ahocoder16_64 "../dataset/data/US_102/Normal/$fname.wav" "../dataset/features/US_102/Normal/f0/$fname.f0" "../dataset/features/US_102/Normal/mcc/$fname.mcc" "../dataset/features/US_102/Normal/fv/$fname.fv"
+./ahocoder16_64 "../dataset/data/Normal/$fname.wav" "../dataset/features/US_102/Normal/f0/$fname.f0" "../dataset/features/US_102/Normal/mcc/$fname.mcc" "../dataset/features/US_102/Normal/fv/$fname.fv"
 done
 
 
 #!/bin/sh
-for entry1 in `ls ../dataset/data/US_102/Whisper/*.wav`;do
+for entry1 in `ls ../dataset/data/Whisper/*.wav`;do
 fname=`basename $entry1 .wav`
 echo $fname 
-./ahocoder16_64 "../dataset/data/US_102/Whisper/$fname.wav" "../dataset/features/US_102/Whisper/f0/$fname.f0" "../dataset/features/US_102/Whisper/mcc/$fname.mcc" "../dataset/features/US_102/Whisper/fv/$fname.fv"
+./ahocoder16_64 "../dataset/data/Whisper/$fname.wav" "../dataset/features/US_102/Whisper/f0/$fname.f0" "../dataset/features/US_102/Whisper/mcc/$fname.mcc" "../dataset/features/US_102/Whisper/fv/$fname.fv"
 done
 exit
