8da18f90364925525b91ec00e983078da643960b
Raj Patel <raj@allsite.ai>
2024-03-05 20:08:18 +1300

    Adding script to ectract baches in sequentially

---
+++ scripts/Run_all_features.m
@@ -0,0 +1,21 @@
+% Master script to run multiple scripts sequentially
+
+% Run the concatenation for VCC2018
+concatenation_VCC2018;
+
+% Run the batch processing
+batch;
+
+% Run the concatenation for f0
+concatenation_f0;
+
+% Run the batch processing for f0
+batch_f0;
+
+% Run the concatenation for V/UV
+concatenation_vuv;
+
+% Run the batch processing for V/UV
+batch_vuv;
+
+% End of master script
